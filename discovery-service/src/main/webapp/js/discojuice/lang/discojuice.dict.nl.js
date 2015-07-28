c4704dba07d541ce8c9f429cf3b4b4aa6e4e45c9
Willem Elbers <willem@clarin.eu>
2015-07-28 11:01:52 +0200

    Vertalingen bijgewerkt

--- discovery-service/src/main/webapp/js/discojuice/lang/discojuice.dict.nl.js
+++ discovery-service/src/main/webapp/js/discojuice/lang/discojuice.dict.nl.js
@@ -10,15 +10,18 @@ DiscoJuice.Dict = {
     "nearby":"Dichtbij",
     "moreLink":"Resultaten beperkt tot %d van de totale %d  \u2013 toon meer",
     "help":"Help, ik kan mijn organisatie niet vinden",
-    "helpMore":"Als uw organisatie niet is aangesloten op deze dienst, dan kunt u ook een account aanmaken bij een van de inlogproviders voor gastgebruik. U kunt ook contact opnemen met uw eigen organisatie en verzoeken om aan te sluiten op deze dienst.",
     "locateMe":"Bepaal waar ik ben en toon nabij gelegen organisaties",
     "locating":"Bezig met bepalen van uw locatie",
     "showIn":"Toon organisaties in",
     "allCountries":"alle landen",
     "showAllCountries":"toon alle landen",
     
-    "connectTo": "Aanmelden bij %s",
     "textHelp": "Externe Identity Providers",
     "textHelpPreselected": "Standaard Identity Provider", 
-    "moreLinkPreselected": "Klik hier om externe Identity Providers te tonen"
+    "moreLinkPreselected": "Klik hier om externe Identity Providers te tonen",
+    
+    "connectTo": "Meld aan via de CLARIN Service Provider Federation",
+    "subtitle": "Selectuur hieronder uw inlogprovider. Dit is meestal de organisatie waar u voor werkt of waar u studeerd. Door uzelf aan te melden krijgt u toegang tot bepaalde CLARIN services welke anders niet toegankelijk zijn.",
+    "helpMore": "Als uw organisatie niet is aangesloten op deze dienst, dan kunt u ook een account aanmaken bij onze <i>clarin.eu website account</i> inlogprovider. U kunt dan gebruik maken van de inloggegevens van de <a href=\"https://user.clarin.eu/user\">CLARIN website</a>. Registreren kan <a href=\"https://user.clarin.eu/user/register\">hier</a>. Als u nog vragen heeft dan kun u contact opnemen via <a href=\"mailto:spf@clarin.eu\">spf@clarin.eu</a>.",
+    "noReturnParamError": "Waarschuwing: Het lijkt erop dat u rechtstreeks deze pagina hebt geopend, dit zal niet werken!. U moet zich aanmelding via de service die u wilt gebruiken.",
 };
