dbddc4cd422098c428b110b0004d9a02f115f3c4
Willem Elbers <willem@clarin.eu>
2016-01-29 17:01:41 +0100

    Bumped version number

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="1.8.3-SNAPSHOT"
+VERSION="1.9.0"
 
 cp discovery-metadata-api/target/metadata-api-${VERSION}.war discovery-service/target/discovery-service-${VERSION}.war .
 tar -pczvf release-${VERSION}.tar.gz *.war
