12b6d1fcc702dbda9dd4669b92d776890e4715a8
Willem Elbers <willem@clarin.eu>
2016-01-29 17:04:59 +0100

    Remove any existing wars before packing a new release

--- release.sh
+++ release.sh
@@ -2,5 +2,6 @@
 
 VERSION="1.9.0"
 
+rm *.war
 cp discovery-metadata-api/target/metadata-api-${VERSION}.war discovery-service/target/discovery-service-${VERSION}.war .
 tar -pczvf release-${VERSION}.tar.gz *.war
