00499280e2b686648f023f3e67618b12a379dd39
Willem Elbers <willem@clarin.eu>
2016-02-09 20:05:28 +0100

    Corrected spelling mistakes as reported by Marlies van Breda (email 2.8.2016)

--- discovery-service/src/main/webapp/js/discojuice/lang/discojuice.dict.nl.js
+++ discovery-service/src/main/webapp/js/discojuice/lang/discojuice.dict.nl.js
@@ -21,7 +21,7 @@ DiscoJuice.Dict = {
     "moreLinkPreselected": "Klik hier om externe Identity Providers te tonen",
     
     "connectTo": "Meld aan via de CLARIN Service Provider Federation",
-    "subtitle": "Selectuur hieronder uw inlogprovider. Dit is meestal de organisatie waar u voor werkt of waar u studeerd. Door uzelf aan te melden krijgt u toegang tot bepaalde CLARIN services welke anders niet toegankelijk zijn.",
+    "subtitle": "Selecteer hieronder uw inlogprovider. Dit is meestal de organisatie waar u voor werkt of waar u studeert. Door uzelf aan te melden krijgt u toegang tot bepaalde CLARIN services welke anders niet toegankelijk zijn.",
     "helpMore": "Als uw organisatie niet is aangesloten op deze dienst, dan kunt u ook een account aanmaken bij onze <i>clarin.eu website account</i> inlogprovider. U kunt dan gebruik maken van de inloggegevens van de <a href=\"https://user.clarin.eu/user\">CLARIN website</a>. Registreren kan <a href=\"https://user.clarin.eu/user/register\">hier</a>. Als u nog vragen heeft dan kun u contact opnemen via <a href=\"mailto:spf@clarin.eu\">spf@clarin.eu</a>.",
     "noReturnParamError": "Waarschuwing: Het lijkt erop dat u rechtstreeks deze pagina hebt geopend, dit zal niet werken. U moet zich aanmelding  <a href=\"https://www.clarin.eu/content/easy-access-protected-resources\">via de service</a> die u wilt gebruiken.",
 };
